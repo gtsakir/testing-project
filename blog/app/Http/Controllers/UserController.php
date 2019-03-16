310d07427093ef1ff780274cd93380dc4e0d825f
gabriel <gtsakir@gmail.com>
2019-03-16 18:25:39 +0200

    initial commit

---
+++ blog/app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabriel
+ * Date: 2019-03-16
+ * Time: 17:30
+ */
+
+namespace App\Http\Controllers;
+
+
+class UserController
+{
+    public function show($id)
+    {
+        return view('welcome', ['totos' => $id]);
+    }
+}
